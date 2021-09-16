5c546f3c0a8aa963024cc436b829d1cf05e205ed
FromTheFarNorth <52026538+YuXinyang-Victor@users.noreply.github.com>
2021-09-16 15:55:03 +0800

    added touch related trust change and msg functions

--- TGbot/talk.py
+++ TGbot/talk.py
@@ -3,7 +3,7 @@ import fileIO as IO
 from random import randint
 
 import other_response as other
-import trust_increase_messages as tim
+import trust_messages as tim
 import new_unlocked_message_alert as numa
 
 trust1 = 400
@@ -13,11 +13,11 @@ trust4 = 2000
 
 def should_i_respond(user_id):
     condition_talk = IO.get_talk_cnt(user_id)
-    condition_harass = IO.get_harass(user_id)
+    condition_touch = IO.get_touch(user_id)
     condition_others = True                     #currently hard-coded as true. This is placeholder that indicates that future addition of conditions can be done here. 
 
 
-    if((condition_talk < 20) & (condition_harass != 2) & condition_others):
+    if((condition_talk < 20) & (condition_touch != 2) & condition_others):
         return True
     else:
         return False
@@ -35,6 +35,14 @@ def rnd_trust_incrs():
     value = randint(1, 10)
     return value
 
+def rnd_trust_incrs_by_touch():
+    value = randint(5, 20)
+    return value
+
+def rnd_trust_decrs_by_touch():
+    value = randint(5, 20)
+    return value
+
 def should_i_alert_new_unlocked_msg(curr_trust1, curr_trust2):
     trust_list = [trust1, trust2, trust3, trust4]
 
@@ -49,7 +57,22 @@ def trust_increase_msg():                           #need to write
     response = tim.trust_increase_message[value]
     return response
 
+def trust_decrease_msg():
+    value = randint(0, len(tim.trust_decrease_message)-1)
+    response = tim.trust_decrease_message[value]
+    return response
+
 def new_unlocked_msg_alert():                   #need to write
     value = randint(0, len(numa.new_unlocked_message_alert)-1)
     response = numa.new_unlocked_message_alert[value]
+    return response
+
+def trust_increase_by_touch_msg():
+    value = randint(0, len(tim.trust_increase_by_touch_message)-1)
+    response = tim.trust_increase_by_touch_message[value]
+    return response
+
+def touch_failed_msg():
+    value = randint(0, len(tim.touch_failed_message)-1)
+    response = tim.touch_failed_message[value]
     return response
