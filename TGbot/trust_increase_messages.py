b3835149b2220c5e6401de4fbd98b288e3ca71dc
FromTheFarNorth <52026538+YuXinyang-Victor@users.noreply.github.com>
2021-09-16 15:53:10 +0800

    updated name; added trust msgs related to touch

--- TGbot/trust_increase_messages.py
+++
@@ -1,4 +0,0 @@
-trust_increase_message = ["(trust_increase_message_placeholder1)", 
-                          "(trust_increase_message_placeholder2)", 
-                          "(trust_increase_message_placeholder3)"]
-#need to write more!
