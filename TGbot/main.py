a7198c5b62dcca9f6be470080df865aff0be95bc
FromTheFarNorth <52026538+YuXinyang-Victor@users.noreply.github.com>
2021-09-21 17:07:10 +0800

    rewrote main with pytelebot api

--- TGbot/main.py
+++ TGbot/main.py
@@ -1,67 +1,47 @@
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-import constants
-import telegram.ext as tg
+import telebot
+import constants as cons
 import responses as resp
 import commands_response as cr
-import fileIO as IO
-import time
-import schedule
+import logging
 
-print("Bot started. ")              #signal that bot started in python commandline
+logger = telebot.logger
+telebot.logger.setLevel(logging.DEBUG) # Outputs debug messages to console.
+logging.basicConfig(filename = "log_for_sept_21.log")
 
-def start_command_handler(update, context):
-    update.message.reply_text(cr.start_response)                       
+bot = telebot.TeleBot(cons.API_KEY)
 
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(message.chat.id, cr.start_response)
 
-def help_command_handler(update, context):
-    update.message.reply_text(cr.help_response)                       
+@bot.message_handler(commands=['help'])
+def send_help(message):
+	bot.send_message(message.chat.id, cr.help_response)
 
-def talk_command_handler(update, context):
-    user_id = update.message.from_user.id
-    talk_response = cr.talk_response(user_id)
-    update.message.reply_text(talk_response)
+@bot.message_handler(commands=['touch'])
+def reply_touch(message):
+	user_id = message.from_user.id
+	touch_response = cr.touch_response(user_id)
+	bot.send_message(message.chat.id, touch_response)
 
-def touch_command_handler(update, context):
-    user_id = update.message.from_user.id
-    touch_response = cr.touch_response(user_id)
-    update.message.reply_text(touch_response) 
+@bot.message_handler(commands=['talk'])
+def reply_talk(message):
+	user_id = message.from_user.id
+	talk_response = cr.talk_response(user_id)
+	bot.send_message(message.chat.id, talk_response)
 
-def check_trust_command_handler(update, context):
-    user_id = update.message.from_user.id
-    check_trust_response = cr.check_trust_response(user_id)
-    update.message.reply_text(check_trust_response)
+@bot.message_handler(commands=['check_trust'])
+def reply_check_trust(message):
+	user_id = message.from_user.id
+	check_trust_response = cr.check_trust_response(user_id)
+	bot.send_message(message.chat.id, check_trust_response)
 
-def msg_handle_interface(update, context):
-    received_text = str(update.message.text).lower()
-    response_text = resp.respond(received_text)
+@bot.message_handler(func=lambda message: True)
+def reply_text_msg(message):
+	received_text = str(message.text).lower()
+	response_text = resp.respond(received_text)
+	bot.send_message(message.chat.id, response_text)
 
-    update.message.reply_text(response_text)
+print("bot started...")
 
-def error(update, context):
-    print(f"update {update} caused error {context.error}")
-
-def main(): 
-    updater = tg.Updater(constants.API_KEY, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start_command_handler))
-    dp.add_handler(CommandHandler("help", help_command_handler))
-    dp.add_handler(CommandHandler("talk", talk_command_handler))
-    dp.add_handler(CommandHandler("touch", touch_command_handler))
-    dp.add_handler(CommandHandler("check_trust", check_trust_command_handler))
-
-    dp.add_handler(MessageHandler(tg.Filters.text, msg_handle_interface))
-
-    dp.add_error_handler(error)
-
-    updater.start_polling()
-    updater.idle()
-
-    schedule.every().day.at("04:00").do(IO.reset_everyday)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(0.01)
-
-main()
+bot.infinity_polling()
